01b691be7a696b641c4495d21aa4bd53e4d60419
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-12-10 21:18:44 +0100

    Premier dataset

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
 # Exemple avec Alpaca FR
-dataset = load_dataset("baconnier/alpaca-fr", split="train")
+dataset = load_dataset("croissantllm/croissant_dataset", split="train")
 
 # Sauvegarder en JSON
-dataset.to_json("mon_dataset_entrainement.json")
+dataset.to_json("data/data-one.json")
