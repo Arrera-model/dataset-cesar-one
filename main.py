b922df75cc8672e0b205f19afdfb52eab9fc5370
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-10 21:33:01 +0100

    Ajout des data set pour le fine-tuning

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from datasets import load_dataset
 
 # Pre-entrainement
-first_train_one = load_dataset("croissantllm/croissant_dataset", split="train")
+first_train_one = load_dataset("croissantllm/croissant_dataset",split="train")
 first_train_two = load_dataset("oscar-corpus/OSCAR-2301", split="train")
 
 first_train_one.to_json("first-train/data-one.json")
 first_train_two.to_json("first-train/data-tow.json")
+
+# Fine-tuning
+
+second_train_one = load_dataset("jpacifico/French-Alpaca-dataset-Instruct-55K",split="train")
+second_train_two = load_dataset("OpenAssistant/oasst1",split="train")
+second_train_theer = load_dataset("PhilSad/Instruct-fr-merged",split="train")
+
+second_train_one.to_json("second-train/data-one.json")
+second_train_two.to_json("second-train/data-two.json")
+second_train_theer.to_json("second-train/data-theer.json")
 
