897a850c3f45c3c1cd1fda1fe25abd8b5593230b
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-10 21:40:28 +0100

    Ajout utf8

--- fine_tunning.py
+++ fine_tunning.py
@@ -5,6 +5,6 @@ second_train_one = load_dataset("jpacifico/French-Alpaca-dataset-Instruct-55K",s
 second_train_two = load_dataset("OpenAssistant/oasst1",split="train")
 second_train_theer = load_dataset("PhilSad/Instruct-fr-merged",split="train")
 
-second_train_one.to_json("second-train/data-one.json")
-second_train_two.to_json("second-train/data-two.json")
-second_train_theer.to_json("second-train/data-theer.json")
+second_train_one.to_json("second-train/data-one.json", force_ascii=False)
+second_train_two.to_json("second-train/data-two.json", force_ascii=False)
+second_train_theer.to_json("second-train/data-theer.json", force_ascii=False)
--- pre_train.py
+++ pre_train.py
@@ -4,5 +4,5 @@ from datasets import load_dataset
 first_train_one = load_dataset("croissantllm/croissant_dataset",split="train")
 first_train_two = load_dataset("oscar-corpus/OSCAR-2301", split="train")
 
-first_train_one.to_json("first-train/data-one.json")
-first_train_two.to_json("first-train/data-tow.json")
+first_train_one.to_json("first-train/data-one.json", force_ascii=False)
+first_train_two.to_json("first-train/data-tow.json", force_ascii=False)
