667a54c1f610c5d66670f67101ec3b87925b31a5
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-10 21:27:07 +0100

    Mise en place des script pour download les dataset de pre-entrainement

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from datasets import load_dataset
 
-# Exemple avec Alpaca FR
-dataset = load_dataset("croissantllm/croissant_dataset", split="train")
+# Pre-entrainement
+first_train_one = load_dataset("croissantllm/croissant_dataset", split="train")
+first_train_two = load_dataset("oscar-corpus/OSCAR-2301", split="train")
 
-# Sauvegarder en JSON
-dataset.to_json("data/data-one.json")
+first_train_one.to_json("first-train/data-one.json")
+first_train_two.to_json("first-train/data-tow.json")
+
